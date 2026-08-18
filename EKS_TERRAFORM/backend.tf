5e3c43051bc1eea8e7e6f4163db9de71fb56d051
subhrajitsahoo72-in <subhrajitsahoo72@gmail.com>
2026-08-18 12:48:35 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-2048-reyaz-4pm" # Replace with your actual S3 bucket name
+    bucket = "terraform-bucket-122" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
